856d22dd0fa238463b3b303c624496a60ae853e6
Olivier3456 <olivier.larrey@gmail.com>
2023-04-26 10:22:03 +0200

    Code displays on the screen

--- Assets/Scripts/NumberPad.cs
+++ Assets/Scripts/NumberPad.cs
@@ -1,41 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NumberPad : MonoBehaviour
 {
     [SerializeField] private int[] goodCode;
 
+    [SerializeField] private TextMeshProUGUI codeDisplay;
+
+
+    [SerializeField] private AudioClip goodCodeAudioClip;
+    [SerializeField] private AudioClip wrongCodeAudioClip;
+
+
     private int[] codeEntered;
 
     private int index = 0;
 
+    private bool goodCodeEntered = false;
+
+    private AudioSource audioSource;
 
     void Start()
     {
         codeEntered = new int[goodCode.Length];
+        audioSource = GetComponent<AudioSource>();
+        ChangeCodeDisplayColor(Color.white);
     }
 
 
     public void ButtonPressed(int buttonValue)
     {
-        codeEntered[index] = buttonValue;
-        index++;      
-        Debug.Log(buttonValue);
-
-        if (index == goodCode.Length)
+        if (!goodCodeEntered)
         {
-            if (CheckCode())
+            codeEntered[index] = buttonValue;
+            index++;
+            ChangeCodeDisplayColor(Color.white);
+            Debug.Log(buttonValue);
+
+            DisplayCode();
+
+            if (index == goodCode.Length)
             {
-                GoodCode();
+                if (CheckCode())
+                {
+                    GoodCode();
+                }
+                else
+                {
+                    WrongCode();
+                }
             }
-            else
-            {
-                WrongCode();
-            }
-        }
-        
-        
+        }        
     }
 
     bool CheckCode()
@@ -51,16 +68,42 @@ public class NumberPad : MonoBehaviour
     }
 
 
+    void DisplayCode()
+    {
+        codeDisplay.text = "";
+
+        for (int i = 0; i < index; i++)
+        {
+            codeDisplay.text += codeEntered[i];
+        }
+
+    }
+
+
 
     void GoodCode()
     {
         Debug.Log("Bon code !");
+        audioSource.PlayOneShot(goodCodeAudioClip);
+        ChangeCodeDisplayColor(Color.green);
+        goodCodeEntered = true;
     }
 
     void WrongCode()
     {
         index = 0;
         Debug.Log("Mauvais code !");
+        audioSource.PlayOneShot(wrongCodeAudioClip);
+        ChangeCodeDisplayColor(Color.red);        
     }
 
+    void ChangeCodeDisplayColor(Color color)
+    {
+        codeDisplay.material.color = color;
+    }
+
+    private void OnDisable()
+    {
+        ChangeCodeDisplayColor(Color.white);
+    }
 }
